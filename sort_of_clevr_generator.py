699f7686682bcb8fa02097b27f873ab1d9f984ce
shamit <shamitlal@yahoo.com>
2017-07-01 17:31:46 +0530

    Fix train and test set size

--- sort_of_clevr_generator.py
+++ sort_of_clevr_generator.py
@@ -4,8 +4,8 @@ import numpy as np
 import random
 import cPickle as pickle
 
-train_size = 8
-test_size = 8
+train_size = 9800
+test_size = 200
 img_size = 75
 size = 5
 question_size = 11 ##6 for one-hot vector of color, 2 for question type, 3 for question subtype
